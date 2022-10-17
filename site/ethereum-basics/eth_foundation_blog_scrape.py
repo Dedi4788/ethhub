1307ad0cebf8e4b2f8efcbd99e0c1e7200132c2b
Sourcery AI <>
2022-10-17 02:39:07 +0000

    'Refactored by Sourcery'

--- site/ethereum-basics/eth_foundation_blog_scrape.py
+++ site/ethereum-basics/eth_foundation_blog_scrape.py
@@ -4,6 +4,7 @@ Created on Tue Sep 24 16:40:32 2019
 @author: SAVANT.SPECTER
 """
 
+
 from pathlib import Path
 import re
 from datetime import date
@@ -23,16 +24,16 @@ while not stop:                                                                #
     post_list = browser.find_elements_by_class_name("post-preview")            #get a list of all posts. They all have the class "post-preview"
     for post in post_list:
         sub_ele = post.find_element_by_tag_name("a")                           #get the sub element for hte link
-        
+
         title = sub_ele.find_element_by_tag_name("h2").text                    #pull the title from a second sub element
         link = sub_ele.get_attribute("href")                                   #get the link
         date_ele = re.search("(\\d{4})/(\\d{1,2})/(\\d{1,2})",link)            #from the link determine the date -- this uses a simple regular expression
-        
-        year = int(date_ele.group(1))
-        month = int(date_ele.group(2))
-        day = int(date_ele.group(3))    
+
+        year = int(date_ele[1])
+        month = int(date_ele[2])
+        day = int(date_ele[3])
         date1 = date(year,month,day)                                           #convert the date to a date obj for easier processing
-        
+
         blog_posts.append((date1,title,link))                                  #add it to the link of all posts        
     try:                                                                       #selenium, when it can't find an element, throws an error - look for this error
         outer_posts_block = browser.find_element_by_class_name("next")         #try to find an "older posts" button
@@ -48,12 +49,9 @@ df = pd.DataFrame(blog_posts,columns = ["date","title","link"])                #
 
 new_file = "blog_links.md.txt"                                                 #make a new file in the current directory, so no old files are changed programmatically
 with open(new_file,"w+", encoding="utf-8") as fil:                             #encoding needed to handle the Eth symbol
-    for i in range(df.shape[0]):                                               #format the line as markdown code
-        text = "[%s - %s](%s)" % (
-                df['date'][i].strftime("%Y.%m.%d"),
-                df['title'][i].replace("]","").replace("[",""),
-                df['link'][i]
-                )
+    for i in range(df.shape[0]):                                           #format the line as markdown code
+        text = f"""[{df['date'][i].strftime("%Y.%m.%d")} - {df['title'][i].replace("]", "").replace("[", "")}]({df['link'][i]})"""
+
         print("%d - %s" % (i,text))                                            #print out in case there are any errors - easier to find and debug
         fil.write(text)                                                        #write line to file -- afterward the contents can be easily copy/pasted as desired
         fil.write("\n")
